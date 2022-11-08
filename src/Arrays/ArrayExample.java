dcff612c22cb55348714cd13e87abee0f57b6db0
Güray ARIK <arikguray@gmail.com>
2022-11-08 12:08:09 +0300

    Week 4

--- src/Arrays/ArrayExample.java
+++ src/Arrays/ArrayExample.java
@@ -5,30 +5,36 @@ import java.sql.SQLOutput;
 public class ArrayExample {
 
 
-    public void printFirstElementOfArray(int arr[])
-    {
-        System.out.println("First element of array = " + arr[0]);
+    public void printElementOfArray(int arr[], int index) {
+        System.out.println("First element of array = " + arr[index]);
     }
 
     /*print all array elements with for loop */
     public void printAllElementOfArray(int arr[]) {
         System.out.print("Elements: ");
         for (int i = 0; i < arr.length; i++) {
-            System.out.print( arr[i] + ", ");
+            System.out.print(arr[i] + ", ");
         }
         System.out.println("");
     }
-        //Array Add Methods
+
+    //Array Add Methods
     public void addElementToLast(int newNumber, int arr[]) {
         System.out.println("**Initial Array **");
         printAllElementOfArray(arr);
         //arr[arr.length]=newNumber;
 
         int newArr[] = new int[arr.length + 1];
-        for (int i = 0; i <newArr.length-1 ; i++) {
+        /*int i=0;
+        while(i <newArr.length-1)
+        {
             newArr[i]=arr[i];
+            i++;
+        }*/
+        for (int i = 0; i < newArr.length - 1; i++) {
+            newArr[i] = arr[i];
         }
-        newArr[newArr.length-1]=newNumber;
+        newArr[newArr.length - 1] = newNumber;
         System.out.println("**New Array**");
         printAllElementOfArray(newArr);
     }
@@ -39,9 +45,9 @@ public class ArrayExample {
         //arr[arr.length]=newNumber;
 
         int newArr[] = new int[arr.length + 1];
-        newArr[0]=newNumber;
-        for (int i = 0; i <newArr.length-1 ; i++) {
-            newArr[i+1]=arr[i];
+        newArr[0] = newNumber;
+        for (int i = 0; i < newArr.length - 1; i++) {
+            newArr[i + 1] = arr[i];
         }
 
         System.out.println("**New Array**");
@@ -49,17 +55,37 @@ public class ArrayExample {
     }
 
     //Array Delete An Item At Index
-   public void DeleteElementWithIndex(int index, int arr[]){
-       System.out.println("**Initial Array **");
-       printAllElementOfArray(arr);
+    public void DeleteElementWithIndex(int index, int arr[]) {
+        System.out.println("**Initial Array **");
+        printAllElementOfArray(arr);
+        //int[] arr = {7, 3, 8, 6, 5, 4, 12, 16, 20, 14};
+        //int[] arr = {7, 3, 8, 5, 4, 12, 16, 20, 14, 14};
+        for (int i = index; i < arr.length - 1; i++) {
+            arr[i] = arr[i + 1];
+        }
 
-       for (int i = index; i < arr.length-1 ; i++) {
-           arr[i]=arr[i+1];
-       }
-       arr[arr.length-1]=0;
+        arr[arr.length - 1] = 0;
 
-       System.out.println("**New Array**");
-       printAllElementOfArray(arr);
+        System.out.println("**New Array**");
+        printAllElementOfArray(arr);
+
+    }
+
+    public void DeleteElementWithIndex2(int index, int arr[]) {
+        System.out.println("**Initial Array **");
+        printAllElementOfArray(arr);
+        //int[] arr = {7, 3, 8, 6, 5, 4, 12, 16, 20, 14};
+        //int[] arr = {7, 3, 8, 5, 4, 12, 16, 20, 14, 14};
+        int newArr[] = new int[arr.length - 1];
+        int j=0;
+        for (int i = 0; i < newArr.length - 1; i++) {
+            if (j != index)
+                newArr[i] = arr[j++];
+        }
+
+
+        System.out.println("**New Array**");
+        printAllElementOfArray(arr);
 
     }
 
--- src/Arrays/Main.java
+++ src/Arrays/Main.java
@@ -7,21 +7,33 @@ public class Main {
 
 
         int[] arr = {7, 3, 8, 6, 5, 4, 12, 16, 20, 14};
+/*
+        int arr[]= new int[10];
+        arr[0]=7;
+        arr[10]=50;
+*/
+        //First Homework
+        //Adding new student and List Students
+
+        //Second HomeWork
+        //Find the Element And Delete it.
 
         ArrayExample arrayClass = new ArrayExample();
-        /*
-        arrayClass.printFirstElementOfArray(arr);
-        arrayClass.printAllElementOfArray(arr);
-        arrayClass.addElementToLast(100, arr);
-        arrayClass.addElementToFirst(2, arr);*/
-        arrayClass.DeleteElementWithIndex(3, arr);
-        /*
-        List<Integer> list= new ArrayList<Integer>();
-        list.add(10);
-        list.add(20);
-        list.add(30);
-        System.out.println(Arrays.toString(list.toArray()));
-         */
+
+        //arrayClass.printElementOfArray(arr, 0);
+        //arrayClass.printAllElementOfArray(arr);
+        //arrayClass.addElementToLast(100, arr);
+        //arrayClass.addElementToFirst(2, arr);
+        //arrayClass.DeleteElementWithIndex(3, arr);
+        arrayClass.DeleteElementWithIndex2(2, arr);
+      //ADT List
+//        List<Integer> list= new ArrayList<Integer>();
+//        list.add(10);
+//        list.add(20);
+//        list.add(30);
+//        list.remove(0);
+//        System.out.println(Arrays.toString(list.toArray()));
+
 
 
     }
