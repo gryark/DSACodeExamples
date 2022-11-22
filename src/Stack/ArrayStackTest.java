bacae45936812b5d11d76590d4661d2bdb4d6042
Güray ARIK <arikguray@gmail.com>
2022-11-22 21:35:31 +0300

    Week 4

--- src/Stack/ArrayStackTest.java
+++ src/Stack/ArrayStackTest.java
@@ -1,5 +1,5 @@
 package Stack;
-import java.util.Stack;
+
 public class ArrayStackTest {
     public static void main(String[] args) {
         MyStack<Integer> stackArr = new ArrayStack<>();
@@ -7,20 +7,5 @@ public class ArrayStackTest {
         stackArr.push(10);
         System.out.println(stackArr.size());
         System.out.println(stackArr.pop());
-
-        Stack<Integer> stack=new Stack<>();
-
-        stack.push(10);
-
-        stack.push(20);
-
-        System.out.println(stack.empty());//return false
-
-        stack.pop(); //20 -->
-
-
-
-
-
     }
 }
